d37b270da13a94a5ac2882c532b35e4f5286f9b0
dominikus1993 <dominikus1910@gmail.com>
2016-10-08 22:15:15 +0200

    add DefaultPostgreDbManager

---
+++ src/Domain/Database/Implementations/DefaultPostgreDbManager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Database.Implementations
+{
+    public class DefaultPostgreDbManager
+    {
+        public DefaultPostgreDbManager()
+        {
+        }
+    }
+}
