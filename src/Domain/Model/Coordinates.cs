6340430896246763be7514ec88c8f27ab85de16d
dominikus1993 <dominikus1910@gmail.com>
2016-10-08 14:51:04 +0200

    add Coordinates model

---
+++ src/Domain/Model/Coordinates.cs
@@ -0,0 +1,15 @@
+namespace Core.Domain.Model
+{
+    public class Coordinates
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Z { get; set; }
+
+        public Coordinates()
+        {
+        }
+    }
+}
