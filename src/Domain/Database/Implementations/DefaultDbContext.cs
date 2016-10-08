3fe7bbb472c1adc955e2b10fad921e21373b19e7
dominikus1993 <dominikus1910@gmail.com>
2016-10-08 22:49:56 +0200

    fix

--- src/Domain/Database/Implementations/DefaultDbContext.cs
+++ src/Domain/Database/Implementations/DefaultDbContext.cs
@@ -1,11 +1,18 @@
 using Core.Domain.Database.Interfaces;
+using Core.Domain.Model;
 using Microsoft.EntityFrameworkCore;
 
 namespace Core.Domain.Database.Implementations
 {
     public class DefaultDbContext : DbContext, IDbManager
     {
-        public DefaultDbContext(DbContextOptions<DefaultPostgreDbManager> options) : base(options)
+        public DbSet<Building> Buildings { get; set; }
+
+        public DbSet<FacultyWreaper> FacultyWreapers { get; set; }
+
+        public DbSet<Place> Places { get; set; }
+
+        public DefaultDbContext(DbContextOptions<DefaultDbContext> options) : base(options)
         {
         }
     }
