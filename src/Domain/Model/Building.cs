7d2ee9b224b557a4a96b628074d30d696c0f9749
dominikus1993 <dominikus1910@gmail.com>
2016-10-08 14:44:28 +0200

    add Building model body

--- src/Domain/Model/Building.cs
+++ src/Domain/Model/Building.cs
@@ -1,12 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Core.Domain.Model
 {
     public class Building
     {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Description { get; set; }
+
+        public Faculty[] Faculty { get; set; }
+        
         public Building()
         {
         }
