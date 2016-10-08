3046f256ed6d50a53b7020275de2ab21bbe8b558
dominikus1993 <dominikus1910@gmail.com>
2016-10-08 09:50:12 +0200

    add Result class

---
+++ src/Domain/Model/Result.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace Core.Domain.Model
+{
+    public class Result<T>
+    {
+        public T Value { get; }
+
+        public bool IsSuccess { get; }
+
+        public IEnumerable<string> Messages { get; }
+
+        public Result(T val, bool isSuccess, params string[] messages)
+        {
+            this.Value = val;
+            this.IsSuccess = isSuccess;
+            this.Messages = messages;
+        }
+
+        public static Result<TResult> Wrap<TResult>(TResult result, params string[] messages) where TResult : class
+        {
+            return new Result<TResult>(result, result != null, messages);
+        }
+
+        public static Result<TResult> Wrap<TResult>(TResult value, Func<TResult, bool> predicate,
+            params string[] messages)
+        {
+            return new Result<TResult>(value, predicate(value), messages);
+        }
+
+        public static Result<TResult> WrapValue<TResult>(TResult result, params string[] messages) where TResult : struct
+        {
+            return new Result<TResult>(result, true, messages);
+        }
+
+        public static Result<T> Error(params string[] messages)
+        {
+            return new Result<T>(default(T), false, messages);
+        }
+    }
+}
