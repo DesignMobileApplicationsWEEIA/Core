ffb8f847414999928f35bce7d1b68737aa595e3d
dominikus1993 <dominikus1910@gmail.com>
2016-10-09 20:47:18 +0200

    add PlaceRepository implementation

--- src/Domain/Repositories/Implementations/PlaceRepository.cs
+++ src/Domain/Repositories/Implementations/PlaceRepository.cs
@@ -2,13 +2,76 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Domain.Database.Interfaces;
+using Core.Domain.Model;
+using Core.Domain.Repositories.Interfaces;
+using LanguageExt;
+using static LanguageExt.Prelude;
 
 namespace Core.Domain.Repositories.Implementations
 {
-    public class PlaceRepository
+    public class PlaceRepository : IPlaceRepository
     {
-        public PlaceRepository()
+        private readonly IDbManager _dbManager;
+
+        public PlaceRepository(IDbManager dbManager)
         {
+            _dbManager = dbManager;
+        }
+
+        public Task<Option<bool>> DeleteAsync(Place entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Option<IEnumerable<Place>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Option<IEnumerable<Place>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Option<IEnumerable<Place>> GetBy(Func<Place, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Option<IEnumerable<Place>>> GetByAsync(Func<Place, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Option<bool> Insert(Place entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Option<bool>> InsertAsync(Place entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Option<int> SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Option<int>> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Option<bool> Update(Place entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Option<bool>> UpdateAsync(Place entity)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- src/Domain/Repositories/Interfaces/IRepository.cs
+++ src/Domain/Repositories/Interfaces/IRepository.cs
@@ -2,16 +2,22 @@ using System;
 using System.Collections.Generic;
 using LanguageExt;
 using Core.Domain.Model;
+using System.Threading.Tasks;
 
 namespace Core.Domain.Repositories.Interfaces
 {
     public interface IRepository<TEntity> where TEntity : BaseEntity
     {
         Option<IEnumerable<TEntity>> GetBy(Func<TEntity, bool> predicate);
+        Task<Option<IEnumerable<TEntity>>> GetByAsync(Func<TEntity, bool> predicate);
         Option<IEnumerable<TEntity>> GetAll();
+        Task<Option<IEnumerable<TEntity>>> GetAllAsync();
         Option<bool> Insert(TEntity entity);
+        Task<Option<bool>> InsertAsync(TEntity entity);
         Option<bool> Update(TEntity entity);
-        Option<bool> Delete(TEntity entity);
-        Option<int> SaveChanges();        
+        Task<Option<bool>> UpdateAsync(TEntity entity);
+        Task<Option<bool>> DeleteAsync(TEntity entity);
+        Option<int> SaveChanges();
+        Task<Option<int>> SaveChangesAsync();
     }
 }
