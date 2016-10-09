8ee815a6ade2b4abd4ed9421e7a604cd8b72401c
dominikus1993 <dominikus1910@gmail.com>
2016-10-09 20:41:34 +0200

    small changes

---
+++ src/Domain/Repositories/Implementations/PlaceRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Repositories.Implementations
+{
+    public class PlaceRepository
+    {
+        public PlaceRepository()
+        {
+        }
+    }
+}
--- src/Domain/Repositories/Interfaces/IRepository.cs
+++ src/Domain/Repositories/Interfaces/IRepository.cs
@@ -1,16 +1,17 @@
 using System;
 using System.Collections.Generic;
+using LanguageExt;
 using Core.Domain.Model;
 
 namespace Core.Domain.Repositories.Interfaces
 {
     public interface IRepository<TEntity> where TEntity : BaseEntity
     {
-        IEnumerable<TEntity> GetBy(Func<TEntity, bool> predicate);
-        IEnumerable<TEntity> GetAll();
-        bool Insert(TEntity entity);
-        bool Update(TEntity entity);
-        bool Delete(TEntity entity);
-        int SaveChanges();        
+        Option<IEnumerable<TEntity>> GetBy(Func<TEntity, bool> predicate);
+        Option<IEnumerable<TEntity>> GetAll();
+        Option<bool> Insert(TEntity entity);
+        Option<bool> Update(TEntity entity);
+        Option<bool> Delete(TEntity entity);
+        Option<int> SaveChanges();        
     }
 }
