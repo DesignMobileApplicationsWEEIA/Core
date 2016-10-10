75bb6b5e2f61dbdf3011d901ec65f9b7b5673ed7
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-10-10 16:36:03 +0200

    add unit of work pattern

---
+++ src/Domain/Repositories/Implementations/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Core.Domain.Database.Interfaces;
+using Core.Domain.Repositories.Interfaces;
+
+namespace Domain.Repositories.Implementations
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbManager _dbManager;
+
+        public UnitOfWork(IDbManager dbManager)
+        {
+            _dbManager = dbManager;
+            PlaceRepository = new PlaceRepository(_dbManager);
+        }
+
+        public IPlaceRepository PlaceRepository { get; }
+        public int Complete()
+        {
+            return _dbManager.SaveChanges();
+        }
+    }
+}
---
+++ src/Domain/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Domain.Repositories.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IPlaceRepository PlaceRepository { get; }
+        int Complete();
+    }
+}
