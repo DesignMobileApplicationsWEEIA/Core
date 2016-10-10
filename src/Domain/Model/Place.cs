87a6c7000ab63a8406c3dd453b6e215a8e1615cc
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-10-10 13:41:37 +0200

    add Equality memebers overriding to Place entity

--- src/Domain/Model/Place.cs
+++ src/Domain/Model/Place.cs
@@ -15,5 +15,35 @@ namespace Core.Domain.Model
         public Building Building { get; set; }
 
         public Guid BuildingId { get; set; }
+
+        private bool Equals(Place other)
+        {
+            return Id.Equals(other.Id) && string.Equals(Name, other.Name) && Latitude.Equals(other.Latitude) && Longitude.Equals(other.Longitude) && BuildingId.Equals(other.BuildingId);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            var a = obj as Place;
+            return a != null && Equals(a);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = Id.GetHashCode();
+                hashCode = (hashCode*397) ^ (Name?.GetHashCode() ?? 0);
+                hashCode = (hashCode*397) ^ Latitude.GetHashCode();
+                hashCode = (hashCode*397) ^ Longitude.GetHashCode();
+                hashCode = (hashCode*397) ^ BuildingId.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Place place1, Place place2) => place1 != null && place1.Equals(place2);
+
+        public static bool operator !=(Place place1, Place place2) => !(place1 == place2);
     }
 }
