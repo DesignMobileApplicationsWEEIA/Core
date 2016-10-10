464aaee8e69291920b54e80c3de0263c957758f9
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-10-10 16:21:24 +0200

    add place repository

---
+++ src/Domain/Repositories/Implementations/PlaceRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Domain.Database.Interfaces;
+using Core.Domain.Model;
+using Core.Domain.Repositories.Interfaces;
+using Domain.Repositories.Abstractions;
+
+namespace Domain.Repositories.Implementations
+{
+    public class PlaceRepository : Repository<Place>, IPlaceRepository
+    {
+        public PlaceRepository(IDbManager dbManager) : base(dbManager)
+        {
+        }
+
+        public override void Dispose()
+        {
+            DbManager?.Dispose();
+        }
+    }
+}
