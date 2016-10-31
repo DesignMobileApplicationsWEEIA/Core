6de38c311054020d490621b839be76cff4f2c88b
dominikus1993 <dominikus1910@gmail.com>
2016-10-31 16:58:41 +0100

    add ICacheService interface

--- src/Domain/Cache/Interfaces/ICacheService.cs
+++ src/Domain/Cache/Interfaces/ICacheService.cs
@@ -1,7 +1,11 @@
-﻿namespace Domain.Cache.Interfaces
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Domain.Cache.Interfaces
 {
     public interface ICacheService
     {
-
+        T GetOrStore<T>(string key, Func<T> func, TimeSpan timeForCache);
+        Task<T> GetOrStoreAsync<T>(string key, Func<Task<T>> func, TimeSpan timeForCache);
     }
 }
