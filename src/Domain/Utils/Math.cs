6725087c1ee2a41e8fa1a2ab443fe17516903c9c
dominikus1993 <dominikus1910@gmail.com>
2016-11-02 21:16:41 +0100

    add Sign method to math utils

--- src/Domain/Utils/Math.cs
+++ src/Domain/Utils/Math.cs
@@ -4,6 +4,9 @@ namespace Domain.Utils
 {
     public static class Math
     {
-        public double
+        public static double Sign(Point p1, Point p2, Point p3)
+        {
+            return (p1.X - p3.X) * (p2.Y - p3.Y) - (p2.X - p3.X) * (p1.Y - p3.Y);
+        }
     }
 }
