57b9c9298822fc63fce181ea04ae2219b30e6bda
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-20 19:57:47 +0100

    add PlaceService and remove DefaultDbContext

---
+++ src/Domain/Services/Implementations/PlaceService.cs
@@ -0,0 +1,30 @@
+﻿using Domain.ApiModel;
+using Domain.Model;
+using Domain.Repositories.Interfaces;
+using Domain.Services.Interfaces;
+
+namespace Domain.Services.Implementations
+{
+    public class PlaceService : IPlaceService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private bool _shouldBeDisposed = true;
+
+        public PlaceService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Dispose()
+        {
+            if (!_shouldBeDisposed) return;
+            _shouldBeDisposed = false;
+            _unitOfWork?.Dispose();
+        }
+
+        public Result<bool> Add(ApiPlace place, string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
