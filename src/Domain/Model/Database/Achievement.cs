b4b143e695efcfb0138d58dbbe87046e7c008a69
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-22 17:32:21 +0100

    add Achievements

---
+++ src/Domain/Model/Database/Achievement.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Model.Database
+{
+    public class Achievement
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
