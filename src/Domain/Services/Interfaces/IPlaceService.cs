d25f6a64af617d30bab85d165af82ed912b5ea6b
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-23 20:35:10 +0100

    add BuidlingService

--- src/Domain/Services/Interfaces/IPlaceService.cs
+++ src/Domain/Services/Interfaces/IPlaceService.cs
@@ -7,6 +7,6 @@ namespace Domain.Services.Interfaces
     public interface IPlaceService
     {
         Result<bool> Add(ApiPlace place, string userId);
-        Result<IEnumerable<Place>> GetAll(ApiPlace place, string userId)
+        Result<IEnumerable<Place>> GetAll(ApiPlace place, string userId);
     }
 }
