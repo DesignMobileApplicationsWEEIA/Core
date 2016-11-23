8634f21d9b83dce98cc11968c866a86827c33c1c
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-23 21:51:30 +0100

    add Faculty Service implementation

--- src/Domain/Services/Interfaces/IFacultyService.cs
+++ src/Domain/Services/Interfaces/IFacultyService.cs
@@ -1,10 +1,11 @@
-﻿using Domain.Model.Api;
+﻿using System.Collections.Generic;
+using Domain.Model.Api;
 using Domain.Model.Database;
 
 namespace Domain.Services.Interfaces
 {
     public interface IFacultyService : IService
     {
-        Result<Faculty> GetAll();
+        Result<IEnumerable<Faculty>> GetAll();
     }
 }
