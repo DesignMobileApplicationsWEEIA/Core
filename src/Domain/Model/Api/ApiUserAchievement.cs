c08b2e23647e4c9346031af5672cd721d4395b7d
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-25 15:15:32 +0100

    smallc changes

--- src/Domain/Model/Api/ApiUserAchievement.cs
+++ src/Domain/Model/Api/ApiUserAchievement.cs
@@ -1,8 +1,19 @@
-﻿namespace Domain.Model.Api
+﻿using Domain.Model.Database;
+
+namespace Domain.Model.Api
 {
     public class ApiUserAchievement
     {
         public long AchievementId { get; set; }
         public string MacAddress { get; set; }
+
+        public static UserAchievement ToUserAchievement(ApiUserAchievement userAchievement)
+        {
+            return new UserAchievement()
+            {
+                AchievementId = userAchievement.AchievementId,
+                MacAddress = userAchievement.MacAddress
+            };
+        }
     }
 }
--- src/Domain/Services/Implementations/UserAchievementService.cs
+++ src/Domain/Services/Implementations/UserAchievementService.cs
@@ -1,7 +1,41 @@
-﻿namespace Domain.Services.Implementations
+﻿using System.Collections.Generic;
+using System.Linq;
+using Domain.Model.Api;
+using Domain.Model.Database;
+using Domain.Repositories.Interfaces;
+using Domain.Services.Interfaces;
+
+namespace Domain.Services.Implementations
 {
-    public class UserAchievementService
+    public class UserAchievementService : IUserAchievementService
     {
-        
+        private readonly IUnitOfWork _unitOfWork;
+        private bool _shouldBeDisposed = true;
+
+        public UserAchievementService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Dispose()
+        {
+            if (!_shouldBeDisposed) return;
+            _shouldBeDisposed = false;
+            _unitOfWork?.Dispose();
+        }
+
+        public Result<IEnumerable<UserAchievement>> GetAll()
+        {
+            return Result<IEnumerable<UserAchievement>>.Wrap(_unitOfWork.UserAchievements.FindAll());
+        }
+
+        public Result<bool> Add(ApiUserAchievement apiUserAchievement)
+        {
+            if (string.IsNullOrEmpty(apiUserAchievement.MacAddress) || _unitOfWork.Achievements.FindAll().All(x => x.Id != apiUserAchievement.AchievementId))
+            {
+                return Result<bool>.Error();
+            }
+            var result = _unitOfWork.UserAchievements.Add()
+        }
     }
 }
