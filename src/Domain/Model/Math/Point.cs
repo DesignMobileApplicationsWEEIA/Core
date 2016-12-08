3f5df1bbe28f69493c3eec45b70072b050ca27da
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-09 00:12:53 +0100

    make Point as reference type

--- src/Domain/Model/Math/Point.cs
+++ src/Domain/Model/Math/Point.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using Domain.Model.Database;
-
-namespace Domain.Model.Math
+﻿namespace Domain.Model.Math
 {
-    public struct Point
+    public class Point
     {
-        public bool Equals(Point other)
-        {
-            return X.Equals(other.X) && Y.Equals(other.Y);
-        }
-
-
         public Point(double x, double y)
         {
             X = x;
@@ -19,34 +10,5 @@ namespace Domain.Model.Math
 
         public double X { get; }
         public double Y { get; }
-
-        public static implicit operator Point(PhoneLocation phoneData)
-        {
-            return new Point(phoneData.Latitude, phoneData.Longitude);
-        }
-
-        public static bool operator ==(Point point1, Point point2)
-        {
-            return (System.Math.Abs(point1.X - point2.X) < double.Epsilon) && (System.Math.Abs(point1.Y - point2.Y) < double.Epsilon);
-        }
-
-        public static bool operator !=(Point point1, Point point2)
-        {
-            return !(point1 == point2);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            return obj is Point && Equals((Point)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return (X.GetHashCode() * 397) ^ Y.GetHashCode();
-            }
-        }
     }
 }
